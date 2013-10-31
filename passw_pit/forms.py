dd3866b2487f9810c40f67442396e4487663bbaa
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-10-31 13:31:57 +0400

    Login page was composed properly.

--- passw_pit/forms.py
+++ passw_pit/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 
 
-class Login(forms.Form):
+class Form(forms.Form):
+    def __unicode__(self):
+        return self.as_p()
+
+
+class Login(Form):
     email=forms.EmailField()
-    password=forms.CharField()
+    password=forms.CharField(widget=forms.PasswordInput())
     salt=forms.CharField(widget=forms.HiddenInput())
