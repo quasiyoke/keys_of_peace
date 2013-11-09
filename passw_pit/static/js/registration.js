5ebd56aaeeb44ae7af420cf43b41c29ffd01cae4
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-11-09 05:50:21 +0400

    Registration form's validation added.

--- passw_pit/static/js/registration.js
+++ passw_pit/static/js/registration.js
@@ -1,3 +1,30 @@
 jQuery(function($){
-	var form = $('.registration-form').find('input:visible:first').focus();
+	var form = $('.registration-form');
+	form.find('input:visible:first').focus();
+	form.validate({
+		rules: {
+			email: {
+				email: true,
+				required: true
+			},
+			password: {
+				required: true
+			},
+			password_confirmation: {
+				equalTo: '.registration-form [name=password]',
+				required: true
+			}
+		},
+		messages: {
+			email: {
+				required: 'Enter your email address.'
+			},
+			password: {
+				required: 'Enter your password.'
+			},
+			password_confirmation: {
+				required: 'Confirm your password.'
+			}
+		}
+	});
 });
