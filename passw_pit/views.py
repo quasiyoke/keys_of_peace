5dd6845eca110532c937961784dd3555b2971822
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-11-12 13:52:15 +0400

    dashboard.scss was added to renamed home.html

--- passw_pit/views.py
+++ passw_pit/views.py
@@ -4,7 +4,7 @@ from django.views.generic import edit as edit_views
 
 
 class Home(edit_views.FormView):
-    template_name = 'home_login.html'
+    template_name = 'home.html'
     form_class = forms.Login
 
     def get_context_data(self, **kwargs):
