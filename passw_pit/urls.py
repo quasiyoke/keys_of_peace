d39eb483c430103bcaa74e76216b9159800c0228
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-11-12 14:36:48 +0400

    Logout was added to dashboard.

--- passw_pit/urls.py
+++ passw_pit/urls.py
@@ -2,6 +2,7 @@ import api
 import views
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
+from django.contrib.auth import views as auth_views
 
 
 admin.autodiscover()
@@ -10,6 +11,7 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', views.Home.as_view(), name='home'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^register/$', views.Registration.as_view(), name='registration'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(api.v1.urls)),
