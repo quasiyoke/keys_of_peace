dd0aa0a18e72ec8a9eb276cb5580c18ab500cc7d
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-11-19 14:53:57 +0400

    "Email" account field was made visible only when email differs from login.

--- keys_of_peace/static/js/dashboard.js
+++ keys_of_peace/static/js/dashboard.js
@@ -126,11 +126,17 @@
 			var element = $(
 				AccountView.template({
 					login: this.model.get('login'),
-					email: this.model.get('email'),
 					password: this.model.get('password')
 				})
 			);
 			var site = this.model.get('accounter').get('mainSite');
+			var loginRow = element.find('.account-login-row');
+			var email = this.model.get('email');
+			if(email && email !== this.model.get('login')){
+				var emailRow = $('<p>');
+				emailRow.html('Email: ' + email);
+				loginRow.after(emailRow);
+			}
 			if(site){
 				var siteLink = $('<a class="account-accounter-link">')
 					.attr('href', site.get('host'))
