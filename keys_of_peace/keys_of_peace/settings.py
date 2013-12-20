52f0eea55ddcc3d7e8fa51dd819a6694223bf88b
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-12-20 07:32:12 +0400

    django_json_settings library was used to keep production settings.

--- keys_of_peace/keys_of_peace/settings.py
+++ keys_of_peace/keys_of_peace/settings.py
@@ -1,33 +1,39 @@
 """
 Django settings for keys_of_peace project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(t9!klkzu2mne225l=35$4%g9hh0_+ui2q7zcxsar74=g(ol!x'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+STATIC_URL = '/static/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
 ALLOWED_HOSTS = []
 
+# json_settings module allows to keep your project's configuration in JSON file. It replaces all settings defined before this import.
+from json_settings import *
 
-# Application definition
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
 
 INSTALLED_APPS = (
     'keys_of_peace',
@@ -53,43 +59,11 @@ ROOT_URLCONF = 'keys_of_peace.urls'
 
 WSGI_APPLICATION = 'keys_of_peace.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/dev/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
 AUTHENTICATION_BACKENDS = (
     'keys_of_peace.backends.Backend',
     'permission.backends.RoleBackend',
     'permission.backends.PermissionBackend',
 )
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/dev/howto/static-files/
-
-STATIC_URL = '/static/'
-
 
 import mimetypes
 mimetypes.add_type('image/svg+xml', '.svg', True)
