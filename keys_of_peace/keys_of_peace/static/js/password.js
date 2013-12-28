989e3e11b37a50e408809fb48161718789b55953
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-12-28 07:08:14 +0400

    Password input editing bug was fixed.

--- keys_of_peace/keys_of_peace/static/js/password.js
+++ keys_of_peace/keys_of_peace/static/js/password.js
@@ -8,7 +8,7 @@
 		},
 		
 		_create: function(){
-			var target = this.options.target
+			var target = this.options.target;
 			if(!target){
 				target = this.element
 					.wrap('<div>')
@@ -25,6 +25,11 @@
 				this.textElement = $('<input type="text">')
 					.val(this._value)
 				;
+				var commonHandlers = {
+					keyup: '_change'
+				};
+				this._on(this.element, commonHandlers);
+				this._on(this.textElement, commonHandlers);
 			}else{
 				this._value = this.element.html();
 				this.element.html(this._value.replace(/./g, '•'));
@@ -35,16 +40,10 @@
 			this.element
 				.after(this.textElement)
 			;
-			if(this.options.class){
-				this.element.addClass(this.options.class);
-				this.textElement.addClass(this.options.class);
+			if(this.options['class']){
+				this.element.addClass(this.options['class']);
+				this.textElement.addClass(this.options['class']);
 			}
-
-			var commonHandlers = {
-				keyup: '_change'
-			};
-			this._on(this.element, commonHandlers);
-			this._on(this.textElement, commonHandlers);
 			
 			this._setMode();
 		},
@@ -58,10 +57,9 @@
 		},
 
 		_change: function(e){
-			this._value = $(e.target).val();
-			this._trigger('change', e, {
-				value: this._value
-			});
+			this.value(
+				$(e.target).val()
+			);
 		},
 
 		_setOption: function(key, value){
