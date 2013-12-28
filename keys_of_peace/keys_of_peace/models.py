4b91aca57b2c290257bf2b300aa9e268fbba1afb
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-12-28 06:37:05 +0400

    Unused models were removed.

--- keys_of_peace/keys_of_peace/models.py
+++ keys_of_peace/keys_of_peace/models.py
@@ -22,40 +22,6 @@ ALPHABETS = (
     )
 
 ALPHABET_CHOICES = [(k, ' + '.join(v), ) for k, v in ALPHABETS]
-
-
-class Account(models.Model):
-    class Meta:
-        index_together = (
-            ('user', 'accounter', ),
-        )
-    
-    user = models.ForeignKey(auth_models.User)
-    accounter = models.ForeignKey('Accounter')
-    login = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    notes = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_now_add=True)
-
-
-class Accounter(models.Model):
-    user = models.ForeignKey(auth_models.User)
-    main_site = models.OneToOneField('Site', related_name='+')
-    registration = models.URLField(blank=True)
-    login = models.URLField(blank=True)
-    logout = models.URLField(blank=True)
-    password_alphabet = models.PositiveSmallIntegerField(choices=ALPHABET_CHOICES, default=700)
-    password_length = models.PositiveSmallIntegerField(default=20)
-    created = models.DateTimeField(auto_now_add=True)
-
-
-class Site(models.Model):
-    host = models.CharField(max_length=50, primary_key=True)
-    user = models.ForeignKey(auth_models.User)
-    name = models.CharField(max_length=50)
-    accounter = models.ForeignKey('Accounter', related_name='sites')
-    created = models.DateTimeField(auto_now_add=True)
     
 
 class UserProfile(models.Model):
