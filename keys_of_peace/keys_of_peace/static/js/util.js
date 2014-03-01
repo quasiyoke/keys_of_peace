6b0ea0d90da7701cc5800a61f1259d84366e6374
Pyotr Ermishkin <quasiyoke@gmail.com>
2014-03-01 07:44:26 +0400

    EsmaScript 6 `contains` strings' method usage was improved on old browsers.

--- keys_of_peace/keys_of_peace/static/js/util.js
+++ keys_of_peace/keys_of_peace/static/js/util.js
@@ -1,6 +1,14 @@
-_.merge($.fn.qtip.defaults, {
-	position: {
-		my: 'right center',
-		at: 'left center'
-	}
-})
+(function () {
+	'use strict';
+	
+	String.prototype.contains || (String.prototype.contains = function (s) {
+		return this.indexOf(s) >= 0;
+	});
+		
+	_.merge($.fn.qtip.defaults, {
+		position: {
+			my: 'right center',
+			at: 'left center'
+		}
+	});
+})();
