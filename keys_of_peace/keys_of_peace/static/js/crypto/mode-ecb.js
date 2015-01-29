7b7e56a8e895625bc954dff6906a2c1379c3d768
Pyotr Ermishkin <quasiyoke@gmail.com>
2015-01-29 03:46:54 +0300

    CryptoJS was updated to 3.1.2

--- keys_of_peace/keys_of_peace/static/js/crypto/mode-ecb.js
+++ keys_of_peace/keys_of_peace/static/js/crypto/mode-ecb.js
@@ -1,3 +1,9 @@
+/*
+CryptoJS v3.1.2
+code.google.com/p/crypto-js
+(c) 2009-2013 by Jeff Mott. All rights reserved.
+code.google.com/p/crypto-js/wiki/License
+*/
 /**
  * Electronic Codebook block mode.
  */
@@ -18,3 +24,4 @@ CryptoJS.mode.ECB = (function () {
 
     return ECB;
 }());
+
